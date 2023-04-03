fccbc71fea8a10ee1db91117fe0f83a513699d30
PatchRequest <me@danielriebel.com>
2023-04-04 01:42:59 +0200

    much change

--- main.py
+++ main.py
@@ -15,15 +15,13 @@ def main():
     agent = chess_white_ai()
 
     try:
-        agent.policy_net.load_state_dict(torch.load('policy_net.pth'))
-        agent.target_net.load_state_dict(torch.load('target_net.pth'))
+        agent.load_model()
     except:
         pass
 
     if sys.argv[1] == 'train':
         agent.train()
-        torch.save(agent.policy_net.state_dict(), 'policy_net.pth')
-        torch.save(agent.target_net.state_dict(), 'target_net.pth')
+        agent.save_model()
     else:
         agent.play()
         pass
@@ -35,25 +33,21 @@ def main():
 class chess_neural_net(nn.Module):
     def __init__(self):
         super(chess_neural_net, self).__init__()
-        # input is 1x1x64
-        self.fc1 = nn.Linear(320, 512)
-        self.fc2 = nn.Linear(512, 512)
-        self.fc3 = nn.Linear(512, 512)
-        self.fc4 = nn.Linear(512, 128)
-        self.fc5 = nn.Linear(128, 64)
-        self.fc6 = nn.Linear(64, 1)
-
+        self.net = nn.Sequential(
+            # input is 1x1x64
+            nn.Linear(384, 512),
+            nn.ReLU(),
+            nn.Linear(512, 128),
+            nn.Tanh(),
+            nn.Linear(128, 64),
+            nn.Sigmoid(),
+            nn.Linear(64, 1)
+        )
 
 
     def forward(self, x):
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        x = F.relu(self.fc5(x))
-        x = self.fc6(x)
-        return x
+        x = x.view(-1, 384)
+        return self.net(x)
     
 
     
@@ -88,21 +82,16 @@ class chess_white_ai:
         available_moves = list(move.uci() for move in  board.legal_moves)
         if len(available_moves) == 0:
             return None
-        
-        state = self.board_to_tensor(board)
+        board_move_tensors = []
+        for move in available_moves:
+            board_move_tensors.append(self.combine_board_move(board,move))
+
+        board_move_tensors = torch.stack(board_move_tensors)
         if random.random() < self.epsilon:
-            with torch.no_grad():
-                # return the move with the highest eval
-                highest = ""
-                for move in available_moves:
-                    if highest == "":
-                        highest = move
-                    else:
-                        if self.eval_move(move) > self.eval_move(highest):
-                            highest = move
-                return highest
-        else:
             return random.choice(available_moves)
+        else:
+            return available_moves[torch.argmax(self.policy_net(board_move_tensors)).item()]
+      
         
     def get_reward(self,board=None):
         if board == None:
@@ -124,11 +113,27 @@ class chess_white_ai:
                 return -6000
         else:
             return int(eval_stock['value'])
+        
+    def combine_board_move(self,board,move):
+        boardTensor = self.board_to_tensor(board)
+        moveTensor = self.move_to_tensor(move)
+        # board is 1x320
+        # move is 1x64
+        # combined is 1x384
+        return torch.cat((boardTensor,moveTensor),1)
+        
+    def combine_tensorBoard_move(self,tensorBoard,move):
+        tensorBoard = tensorBoard
+        moveTensor = self.move_to_tensor(move)
+        # board is 1x320
+        # move is 1x64
+        # combined is 1x384
+        return torch.cat((tensorBoard,moveTensor),1)
+        
 
+    
     def eval_move(self,move):
-        board = self.board.copy()
-        board.push(chess.Move.from_uci(move))
-        return self.policy_net(self.board_to_tensor(board))
+        return self.policy_net(self.combine_board_move(self.board,move))
     
     def board_to_tensor(self,board=None):
         if board == None:
@@ -166,10 +171,8 @@ class chess_white_ai:
         if rights & chess.BB_A8:
             tensor[0][0][4] = -1
 
-
-            
-
         return tensor.view(1,320)
+    
     def move_to_tensor(self,move):
         tensor = torch.zeros(8,8)
         tensor[ord(move[0])-97][int(move[1])-1] = -1
@@ -184,60 +187,80 @@ class chess_white_ai:
         episode_lost = 0
         episode_draw = 0
         episode_count = 1002
-        for i in range(episode_count):
-            length = 0
-            self.board.reset()
-            while not self.board.is_game_over():
-                length += 1
-                state = self.board_to_tensor()
-                action = self.select_action()
-                if action == None:
-                    break
-                self.board.push(chess.Move.from_uci(action))
-                next_state = self.board_to_tensor()
-                reward = self.get_reward()
-                memory.append((state, action, next_state, reward))
-                prediction = self.policy_net(next_state)
-                self.optimize_model(memory,optimizer)
-                # use stockfish to make a counter move
-                if not self.board.is_game_over():
-                    if random.random() < .3:
-                        stockfish = Stockfish()
-                        stockfish.set_fen_position(self.board.fen())
-                        move = stockfish.get_best_move()
-                        self.board.push(chess.Move.from_uci(move))
-                    else:
-                        move = random.choice(list(self.board.legal_moves)).uci()
-                        self.board.push(chess.Move.from_uci(move))
-                difference = prediction.item() - reward
-                print("episode: ",i," epsilon: ",self.epsilon," reward: ",reward," length: ",length, "Prediction: ",prediction.item()," Move: ",move, " Difference: ",difference)
-                if i % 10 == 0:
-                    self.target_net.load_state_dict(self.policy_net.state_dict())
-                if i % 100 == 0:
-                    torch.save(self.policy_net.state_dict(), "policy_net.pth")
-                    torch.save(self.target_net.state_dict(), 'target_net.pth')
-            
-            if self.board.result() == "1-0":
-                episode_won += 1
-            elif self.board.result() == "0-1":
-                episode_lost += 1
-            else:
-                episode_draw += 1
-            print(f"Won by checkmate: {episode_won} Lost by checkmate: {episode_lost} Draw: {episode_draw}")
+        difference_avg = 0
+        difference_count = 0
+        # open train.log in append mode
+        with open("train.log", "a") as f:
 
+            for i in range(episode_count):
+                length = 0
+                self.board.reset()
+                while not self.board.is_game_over():
+                    length += 1
+                    state = self.board_to_tensor()
+                    action = self.select_action()
+                    if action == None:
+                        break
+                    self.board.push(chess.Move.from_uci(action))
+                    next_state = self.board_to_tensor()
+                    reward = self.get_reward()
+                    memory.append((state, action, next_state, reward))
+                    prediction = self.eval_move(action)
+                    self.optimize_model(memory,optimizer)
+                    # use stockfish to make a counter move
+                    if not self.board.is_game_over():
+                        if random.random() < .3:
+                            stockfish = Stockfish()
+                            stockfish.set_fen_position(self.board.fen())
+                            move = stockfish.get_best_move()
+                            self.board.push(chess.Move.from_uci(move))
+                        else:
+                            move = random.choice(list(self.board.legal_moves)).uci()
+                            self.board.push(chess.Move.from_uci(move))
+                    difference = prediction.item() - reward
+                    # calculate average difference
+                    difference_count += 1
+                    difference_avg += 1/difference_count * (difference - difference_avg)
+                    
+                    #print(i, " ", difference_avg)
+                    #print("episode: ",i," reward: ",reward," length: ",length, "Prediction: ",prediction.item()," Move: ",move)
+                    print(f"Episode: {i} \t Reward: {reward} \t Length: {length} \t Prediction: {prediction.item()} \t Move: {move} \t AvgDiff: {difference_avg}")
+                    f.write(str(difference_avg)+"\n")
+                    if i % 10 == 0:
+                        self.target_net.load_state_dict(self.policy_net.state_dict())
+                    if i % 100 == 0:
+                        self.save_model()
+                
+                if self.board.result() == "1-0":
+                    episode_won += 1
+                elif self.board.result() == "0-1":
+                    episode_lost += 1
+                else:
+                    episode_draw += 1
+                print(f"Won by checkmate: {episode_won} Lost by checkmate: {episode_lost} Draw: {episode_draw}")
+
+    def save_model(self):
+        torch.save(self.policy_net.state_dict(), "policy_net.pth")
+        torch.save(self.target_net.state_dict(), 'target_net.pth')
+    def load_model(self):
+        self.policy_net.load_state_dict(torch.load("policy_net.pth"))
+        self.target_net.load_state_dict(torch.load("target_net.pth"))
 
     def optimize_model(self,memory,optimizer):
         if len(memory) > 32:
             batch = random.sample(memory, 32)
             for state, action, next_state, reward in batch:
                 target = reward
+                target = torch.tensor([[target]], dtype=torch.float)
                 
-                target = (reward + 0.9 * self.target_net(next_state).max())
-                current = self.policy_net(state).max()
-                loss = F.smooth_l1_loss(current, target)
+                
+                board_move = self.combine_tensorBoard_move(state,action)
+                prediction = self.policy_net(board_move)
+                loss = F.smooth_l1_loss(prediction, target)
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
+
     
 if __name__ == "__main__":
     main()
